d43b40be902f0f5bdfa706566b60ca284fbc8bf8
LinguaBuddy <fadimeselvi888@gmail.com>
2026-08-18 21:27:27 +0300

    Add files via upload

---
+++ models.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------
+# PyEngine Architect M3 - Dynamic Refactored Module
+# ------------------------------------------------------------------
+
+class PyEngineArchitectM3(CTkDnD):
