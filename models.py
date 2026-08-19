2b44b199921c51746594fe7014f6561c490bae4f
LinguaBuddy <fadimeselvi888@gmail.com>
2026-08-19 11:50:33 +0300

    Delete models.py

--- models.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# ------------------------------------------------------------------
-# PyEngine Architect M3 - Dynamic Refactored Module
-# ------------------------------------------------------------------
-
-class PyEngineArchitectM3(CTkDnD):
